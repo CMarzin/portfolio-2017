0c6ade7b9c59c70ca0b00fec1038cc3ced2cb084
CMarzin <corentinmarzin@Gmail.com>
2017-04-01 12:57:03 +0200

    [ADD] add finale animation choose project to project

--- app/js/index.js
+++ app/js/index.js
@@ -3,8 +3,6 @@ import data from "../data/data.json";
 const d = data;
 const about = data.about;
 
-console.log(d);
-
 let project_choose = document.querySelector('#project__choose');
 let project_desc = document.querySelector('#project__desc');
 let page_about = document.querySelector('#about');
@@ -13,12 +11,28 @@ let content_choose = MyApp.templates.projectChoose(d);
 let content_desc = MyApp.templates.projectDesc(d);
 let content_about = MyApp.templates.about(about);
 
-// console.log(content_about);
-// console.log(content);
-
+// Insert json to html
 project_choose.innerHTML = content_choose;
-// project_desc.innerHTML = content_desc;
-// page_about.innerHTML = content_about;
+project_desc.innerHTML = content_desc;
+page_about.innerHTML = content_about;
+
+// Select mask svg
+let rect1 = document.querySelector('.rect1');
+let rect2 = document.querySelector('.rect2');
+let rect3 = document.querySelector('.rect3');
+let rect4 = document.querySelector('.rect4');
+
+let container = document.querySelector('#project__choose');
+let container_svg = document.querySelector('.container');
+
+let see_project = document.querySelector('.container__button--open');
+let container_title = document.querySelector('.container__title--flex');
+let container__arrows = document.querySelector('.container__arrows');
+let container__button = document.querySelector('.container__button');
+let arrows = document.querySelectorAll('.container__arrow');
+
+let bgwhite_left = document.querySelector('.projects__project--left-bg--white');
+let bgwhite_right = document.querySelector('.projects__project--right-bg--white');
 
 /*
 * Anim mask svg
@@ -40,28 +54,16 @@ const move_mask = (e) => {
 document.body.onmousemove = move_mask;
 
 
-// reactive scroll anim
-// document.querySelector('.menu__logo').addEventListener('click', function() {
-//   document.body.onmousemove = move_mask;
-// });
+//reactive scroll anim
+document.querySelector('.menu__logo').addEventListener('click', function() {
+  document.body.onmousemove = move_mask;
+});
 
-
-let rect1 = document.querySelector('.rect1');
-let rect2 = document.querySelector('.rect2');
-let rect3 = document.querySelector('.rect3');
-let rect4 = document.querySelector('.rect4');
-
-let container = document.querySelector('#project__choose');
-let container_svg = document.querySelector('.container');
-
-let see_project = document.querySelector('.container__button--open');
-let container_title = document.querySelector('.container__title--flex');
-let arrows = document.querySelectorAll('.container__arrow');
-
-
+// Launch anim
 see_project.addEventListener('click', function() {
-    // disable e scroll
+    // disable event on scroll
     document.body.onmousemove = null
+
     rect1.classList.add('transition');
     rect2.classList.add('transition');
     rect3.classList.add('transition');
@@ -79,8 +81,24 @@ see_project.addEventListener('click', function() {
                 arrows[1].classList.remove('container__arrow--next');
                 setTimeout(() => {
                     container_svg.classList.add('container--hide');
+                    project_choose.classList.add('display--hide');
+                    container__arrows.classList.add('display--hide');
+                    animation_desc();
                 }, 1000);
             }, 400);
         }, 300);
     }, 200);
 });
+
+
+function animation_desc() {
+  project_desc.classList.remove('display--hide');
+  container__button.classList.remove('display--hide');
+  setTimeout( () => {
+    bgwhite_left.classList.add('projects__project--left-bg--white--hide');
+    bgwhite_right.classList.add('projects__project--right-bg--white--hide');
+    setTimeout( () => {
+
+    },1000);
+  },500);
+}
