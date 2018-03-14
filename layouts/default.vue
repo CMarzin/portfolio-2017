59c8c78b770e2692072b1c71d6b9a8f276941127
CMarzin <corentinmarzin@Gmail.com>
2018-03-14 11:52:07 +0100

    [FIX] fix height descriptionj ump

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,26 +5,28 @@
     <div class="container__left">
       <nuxt-link :class="['container__left-prev-project', customClassBgPrevious]" :to="this.$store.state.pathToPreviousProject"></nuxt-link>
       <div class="container__left-elements">
-        <div class="container__left-counter">
-          <p>{{ this.$store.state.counterProject + 1 }}</p>
-          <p> - </p>
-          <p>{{ this.$store.state.projectTitle.length }}</p>
+        <div class="container__left-elements-content">
+          <div class="container__left-counter">
+            <p>{{ this.$store.state.counterProject + 1 }}</p>
+            <p> - </p>
+            <p>{{ this.$store.state.projectTitle.length }}</p>
+          </div>
+          <div :class="['title', 'container__left-title',customClassText]">
+          <h1>{{ this.$store.state.currentProjectTitle }}</h1>
+          </div>
+          <!-- SHOW ARROW ON MOBILE -->
+          <!-- <div class="container__left-nav">
+            <nuxt-link class="container__left-arrow" :to="this.$store.state.pathToPreviousProject">
+              <span class="container__main-footer_arrow-left"></span>
+            </nuxt-link>
+            <nuxt-link class="container__left-arrow" :to="this.$store.state.pathToNextProject">
+              <span class="container__main-footer_arrow-right"></span>
+            </nuxt-link>
+          </div> -->
         </div>
-        <div :class="['title', 'container__left-title',customClassText]">
-         <h1>{{ this.$store.state.currentProjectTitle }}</h1>
-        </div>
-        <!-- SHOW ARROW ON MOBILE -->
-        <!-- <div class="container__left-nav">
-          <nuxt-link class="container__left-arrow" :to="this.$store.state.pathToPreviousProject">
-            <span class="container__main-footer_arrow-left"></span>
-          </nuxt-link>
-          <nuxt-link class="container__left-arrow" :to="this.$store.state.pathToNextProject">
-            <span class="container__main-footer_arrow-right"></span>
-          </nuxt-link>
-        </div> -->
-        <div class="container__left-desc">
-          {{ this.$store.state.projects[this.$store.state.pathToCurrentProject].description }}
-        </div>
+          <div class="container__left-desc">
+            {{ this.$store.state.projects[this.$store.state.pathToCurrentProject].description }}
+          </div>
       </div>
     </div>
     <div class="container__middle">
